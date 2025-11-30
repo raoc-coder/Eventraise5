bb02c2c90486aaee7c5736841f681dc83917f91a
raoc-coder <raoc@onthemarc.net>
2025-11-30 16:27:55 +0530

    c

--- components/analytics/MetaPixel.tsx
+++ components/analytics/MetaPixel.tsx
@@ -1,35 +1,41 @@
 import Script from 'next/script'
 
+// Get pixel ID at module level (server-side)
 const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID
 
 export function MetaPixel() {
+  // Early return if pixel ID not configured
   if (!META_PIXEL_ID) {
+    if (process.env.NODE_ENV === 'development') {
+      console.warn('[Meta Pixel] NEXT_PUBLIC_META_PIXEL_ID environment variable is not set')
+    }
     return null
   }
 
+  // Standard Meta Pixel code format
+  const pixelCode = `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');`
+
   return (
     <>
-      {/* Meta Pixel Code - Complete inline script for Meta Events Manager detection */}
-      {/* Using beforeInteractive to ensure it's in the initial HTML head */}
+      {/* Meta Pixel Code - Standard implementation */}
+      {/* Note: afterInteractive places script in body, but Meta Events Manager should still detect it */}
       <Script
-        id="meta-pixel"
-        strategy="beforeInteractive"
+        id="facebook-pixel"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${META_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
+          __html: pixelCode,
         }}
       />
-      {/* Noscript fallback - Required for Meta Events Manager detection */}
+      {/* Noscript fallback - REQUIRED for Meta Events Manager detection */}
       <noscript>
         <img
           height="1"
